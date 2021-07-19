9c3131f7291773848e66ee0e5d516b63697c0415
Rajesha Soratoor <rajesh_sortur@yahoo.co.in>
2021-07-19 07:52:56 +0530

    modified home and added new addition class

--- www.org/src/main/java/pages/Home.java
+++ www.org/src/main/java/pages/Home.java
@@ -3,9 +3,16 @@ package pages;
 public class Home 
 
 {
+	public void concat()
+	{
+		String concat;
+		
+	}
 public static void main(String[] args) 
 {
 	System.out.println("Hello");
 	System.out.println("test");
+	
+
 }
 }
